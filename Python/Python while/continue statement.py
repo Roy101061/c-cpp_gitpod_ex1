487d3cc7ef3e07a15663186d64da1fdbde3a4be3
Roy101061 <Roy101061@gmail.com>
2021-12-07 13:10:19 +0000

    update

---
+++ Python/Python while/continue statement.py
@@ -0,0 +1,7 @@
+a = 1
+while a < 10:
+    a += 1
+    if (a == 5):
+        continue #skip
+    print(a)
+    
--- Python/Python while/while loop.py
+++ Python/Python while/while loop.py
@@ -1,4 +1,4 @@
-i = 1
-while i < 6:
-  print(i)
-  i += 1
+a = 1
+while a < 10:
+     print(a)
+     a += 1
