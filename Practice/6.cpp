091c4623cfb930ffad9a94cd6f45605ed5160084
Roy101061 <Roy101061@gmail.com>
2021-12-14 13:42:15 +0000

    update

---
+++ Practice/6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "6_function.h"
+
+using namespace std;
+
+int main(){
+    cin>>a>>b;
+    if(a>b)
+      cout<<a<<endl;
+    else if(a==b)
+        cout<<"兩個一樣大="<<a<<endl;
+    else
+        cout<<b<<endl;
+    return 0;
+}
