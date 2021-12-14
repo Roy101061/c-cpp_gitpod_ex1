71e4e05e76c2c55e371ff6d6e2d04fa3a5c1b6a9
Roy101061 <Roy101061@gmail.com>
2021-12-14 13:30:47 +0000

    update

---
+++ Practice/5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    if(x<0)
+        cout<<"1\n";
+    else if(x==0)
+        cout<<"5\n";
+    else
+        cout<<"3\n";
+	return 0;
+}
