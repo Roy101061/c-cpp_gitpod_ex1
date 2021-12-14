17020f9b6ccccc92f72491e20e253de52ae98fe4
Roy101061 <Roy101061@gmail.com>
2021-12-14 13:55:20 +0000

    update

---
+++ Practice/8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(c==(a+b))
+        cout<<"right\n";
+    else
+        cout<<"error\n";
+    return 0;
+}
