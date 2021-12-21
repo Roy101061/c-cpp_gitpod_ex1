dd428f9563419157e0b50f67544ac5b42e31ae61
Roy101061 <Roy101061@gmail.com>
2021-12-21 12:45:45 +0000

    update

--- Practice/14.cpp
+++ Practice/14.cpp
@@ -1,1 +1,18 @@
-git 
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int pos=0,neg=0,zero=0,a;
+	for(int b=0;b<10;b++){
+		cin>>a;
+		if(a>0)
+			pos++;
+		if(a<0)
+			neg++;
+		if(a==0)
+			zero++;
+	}
+	cout<<"pos:"<<pos<<" neg:"<<neg<<" zero:"<<zero;
+	return 0;
+}
